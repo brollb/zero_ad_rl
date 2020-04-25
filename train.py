730f4fc97cedd0f7b710fd2eede72299c5cb32ac
Brian Broll <brian.broll@gmail.com>
2020-04-25 07:36:53 -0500

    Call on_train_result if defined on env

--- train.py
+++ train.py
@@ -10,8 +10,31 @@ register_env('CavalryVsInfantry', lambda c: CavalryVsInfantryEnv(c))
 register_env('SimpleMinimapCavVsInf', lambda c: SimpleMinimapCavVsInfEnv(c))
 register_env('MinimapCavVsInf', lambda c: MinimapCavVsInfEnv(c))
 
+def invoke_if_defined(obj, method, *args):
+    fn = getattr(obj, method, None)
+    if fn is not None:
+        fn(*args[1:])
+
+
+def on_train_result(info):
+    result = info["result"]
+    reward_mean = result["episode_reward_mean"]
+    trainer = info["trainer"]
+    trainer.workers.foreach_worker(
+        lambda ev: ev.foreach_env(
+            lambda env: invoke_if_defined(env, 'on_train_result', reward_mean)))
+
 if __name__ == '__main__':
     parser = create_parser()
     parser.set_defaults(env='CavalryVsInfantry')
     args = parser.parse_args()
+    config = args.config
+
+    if 'callbacks' not in config:
+        config['callbacks'] = {}
+
+    if 'on_train_result' not in config['callbacks']:
+         config['callbacks']['on_train_result'] = on_train_result
+    else:
+        print('on_train_result defined. Overriding default')
     run(args, parser)
