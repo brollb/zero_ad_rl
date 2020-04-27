1355f86e48cb7a84703be6825b4f965aa0ae7952
Brian Broll <brian.broll@gmail.com>
2020-04-27 09:57:24 -0500

    Register envs

--- rollout.py
+++ rollout.py
@@ -3,9 +3,11 @@
 """
 from ray.rllib.rollout import create_parser, run
 from ray.tune.registry import register_env
-from cav_vs_inf_env import CavalryVsInfantryEnv
+from cav_vs_inf_env import *
 
 register_env('CavalryVsInfantry', lambda c: CavalryVsInfantryEnv(c))
+register_env('SimpleMinimapCavVsInf', lambda c: SimpleMinimapCavVsInfEnv(c))
+register_env('MinimapCavVsInf', lambda c: MinimapCavVsInfEnv(c))
 
 if __name__ == '__main__':
     parser = create_parser()
