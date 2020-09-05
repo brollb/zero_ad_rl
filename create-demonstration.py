05945c08105ee313333124d042f6348b982fdb36
Brian Broll <brian.broll@gmail.com>
2020-09-05 07:43:19 -0500

    Add basic action translation

--- create-demonstration.py
+++ create-demonstration.py
@@ -1,6 +1,3 @@
-# TODO: Load a pretrained agent from a checkpoint
-# TODO: Read in a states file
-
 from zero_ad import GameState
 import ray
 import gym
@@ -9,13 +6,28 @@ from ray.tune.registry import register_env, _global_registry, ENV_CREATOR
 from cav_vs_inf_env import *
 import argparse
 import json
+import pickle
 
 register_env('CavalryVsInfantry', lambda c: CavalryVsInfantryEnv(c))
 register_env('SimpleMinimapCavVsInf', lambda c: SimpleMinimapCavVsInfEnv(c))
 register_env('MinimapCavVsInf', lambda c: MinimapCavVsInfEnv(c))
 
+def walk_target(command):
+    return (command['x'], command['z'])
+
+def distance(p1, p2):
+    return math.sqrt(sum((math.pow(x2 - x1, 2) for (x1, x2) in zip(p1, p2))))
+
 def closest_action(env, command):
-    pass
+    if command['type'] == 'attack':
+        return 8
+
+    possible_actions = [(i, env.resolve_action(i)) for i in range(8)]
+    possible_actions.sort(key=lambda pair: distance(walk_target(pair[1]), walk_target(command)))
+    return possible_actions[0][0]
+
+def is_game_over(state):
+    return any([player['state'] != 'active' for player in state.data['players']])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -49,20 +61,25 @@ if __name__ == '__main__':
         states = (GameState(json.loads(line), env.game) for line in states_file)
         trajectory = []
         for state in states:
+            if is_game_over(state):
+                continue
+
             env.prev_state = env.state
             env.state = state
             obs = env.observation(state)
             action = agent.compute_action(obs)
             command = env.resolve_action(action)
             if target_env:
+                target_env.prev_state = target_env.state
+                target_env.state = state
                 obs = target_env.observation(state)
                 action = closest_action(target_env, command)
 
-            if prev_obs:
+            if prev_obs is not None:
                 trajectory.append([prev_obs, prev_action, obs])
 
             prev_obs = obs
             prev_action = action
         trajectories.append(trajectory)
 
-    pickle.dump(open(args.out, 'wb'), trajectories)
+    pickle.dump(trajectories, open(args.out, 'wb'))
